2e41083c3b8e3dfc8b588dd089fa3bc00aff52d3
JoannaMunyasia <joanna.munyasia@strathmore.edu>
2023-03-22 11:24:45 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *_isalpha:checks for letters of the alphabet
- *@c:Letter to be checked
- *Return:one if letter, zero otherwise
+*_isalpha:checks for letters of the alphabet
+*@c:Letter to be checked
+*Return:one if letter, zero otherwise
 */
 int _isalpha(int c)
 {
-	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <='Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
