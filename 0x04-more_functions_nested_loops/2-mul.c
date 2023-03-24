d2a4a3684f7e92d3a3b446023aea131712224df9
JoannaMunyasia <joanna.munyasia@strathmore.edu>
2023-03-24 12:09:06 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul-Multiplies two numbers
+ *@a: Parameter 1
+ *@b: Parameter 2
+ *Return:Result of a*b
+*/
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
