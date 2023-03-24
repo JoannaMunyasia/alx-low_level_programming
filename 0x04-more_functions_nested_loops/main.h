d29970484c863fc34f9ef0d36dd92b0c75099c60
JoannaMunyasia <joanna.munyasia@strathmore.edu>
2023-03-24 12:14:38 -0700

    Task 2

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,11 @@
 #define MAIN
 extern int _isupper(int c);
 extern int _isdigit(int c);
+extern int mul(int a,int b);
+extern void print_most_numbers(void);
+extern void more_numbers(void);
+extern void print_line(int n);
+extern void print_diagonal(int n);
+extern void print_square(int size);
+
 #endif
